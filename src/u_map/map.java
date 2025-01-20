818d0f9c80767ba11d1010d0f20becded2667c2e
nr3278 <ren.20010518@gmail.com>
2025-01-20 13:47:58 +0900

    map.java

--- src/u_map/map.java
+++ src/u_map/map.java
@@ -1,101 +1,24 @@
-<<<<<<< HEAD
 package u_map;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/user/map" })
-public class map extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+import tool.CommonServlet;
 
+@WebServlet(urlPatterns={"/user/map"})
+public class map extends CommonServlet {
 
-//		StoresDAO dao = new StoresDAO();
-		// 店舗情報取得DAO
-		try {
-//			List<Stores> s_list = dao.all();
+    @Override
+    protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        req.getRequestDispatcher("/user/map.jsp").forward(req, resp);
 
+    }
 
+    @Override
+    protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
 
-
-
-			// 表示データリストをjspに
-//			req.setAttribute("storesList", s_list);
-			req.getRequestDispatcher("/user/map.jsp").forward(req, resp);
-
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-
-			System.out.print("DAOでエラー");
-		}
-
-
-
-	}
+    }
 
 }
-=======
-package u_map;
-
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import bean.Stores;
-import dao.StoresDAO;
-
-@WebServlet(urlPatterns = { "/user/map" })
-public class map extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-		StoresDAO dao = new StoresDAO();
-		// 店舗情報取得DAO
-		try {
-			List<Stores> s_list = dao.all();
-
-
-
-
-            for (Stores store : s_list) {
-                String groupCode = store.getName();
-                double a = store.getLatitude();
-
-                System.out.println(groupCode);
-                System.out.println(a);
-
-
-            }
-
-
-
-
-			// 表示データリストをjspに
-			req.setAttribute("storesList", s_list);
-			req.getRequestDispatcher("/user/map.jsp").forward(req, resp);
-
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-
-			System.out.print("DAOでエラー");
-		}
-
-
-
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/hiromu-yatsuda/meal-mate.git
