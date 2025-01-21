115842ea5d4fed3fcd7dfbfd643b7c4bd11567f3
nr3278 <ren.20010518@gmail.com>
2025-01-21 15:23:39 +0900

    commit

--- WebContent/static/ar.js
+++ WebContent/static/ar.js
@@ -1,4 +1,10 @@
 let obj = {};
+let time = 0;
+const timeAllowed = 20;
+let res;
+let imageArray = [];
+let prevBarcode = "";
+const frameRate = 10;
 
 // Quagga
 Quagga.init(
@@ -24,32 +30,42 @@ Quagga.init(
 );
 
 function getFoods(barcode) {
-	$.ajax({
-		url: "/meal-mate/ajax-test",
-		type: "GET",
-		data: {
-			"barcode": barcode
-		},
-		dataType: "json"
-	}).done(function (response) {
-		let foodText = "";
-		for (let item of response.classes) {
-			foodText += item + "\n";
-		}
-		// 恐らくitemは画像path
-		// daoから返される名称と画像の名前をそろえる
-		text = foodText;
-	})
+	if (prevBarcode != barcode) {
+		prevBarcode = barcode;
+		imageArray = [];
+		$.ajax({
+			url: "/meal-mate/ajax-test",
+			type: "GET",
+			data: {
+				"barcode": barcode
+			},
+			dataType: "json"
+		}).done(function (res) {
+			loadFoods(res["paths"]);
+			// 恐らくitemは画像path
+			// daoから返される名称と画像の名前をそろえる
+		}).fail(function (res) {
+			console.log(res);
+		})
+	}
 }
 
-function checkBarcode(num) {
+function loadFoods(foodsProp) {
+	for (let item of foodsProp) {
+		const img = new Image();
+		img.src = `/meal-mate/img/${ item }`;
+		imageArray.push(img);
+	}
+
+}
+
+async function checkBarcode(num) {
     for (let key in obj) {
         if (key == num) {
             obj[num] += 1;
-            if (obj[num] >= 7) {
-                getFoods(num);
-                console.log(text);
+            if (obj[num] >= 10) {
                 obj = {};
+                getFoods(num);
             }
             return;
         }
@@ -62,46 +78,55 @@ function calcRect(box) {
     const x1 = box[1][0];
     const x2 = box[2][0];
     const x = (x1 + x2) / 2 + xDistance;
-    const yDistance = 100;
+    const yDistance = -100;
     const y1 = box[1][1];
     const y = y1 + yDistance;
 
     return [x, y];
 }
 
-function drawText(result, foods) {
+function drawText() {
     const ctx = Quagga.canvas.ctx.overlay;
     const canvas = Quagga.canvas.dom.overlay;
-    if (result == null) {
-        arText = "";
-        ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
-        console.log("none");
+    ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
+    if (res == null) {
         return;
     }
-    if (typeof result != "object") {
+    if (typeof res != "object") {
+    	return
     }
-    if (result.boxes == undefined) {
+    if (res.boxes == undefined) {
+    	return
     }
-    ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
 
-    const rect = calcRect(result.box);
+    const rect = calcRect(res.box);
 
-    ctx.font = "100px serif";
-    ctx.fillStyle = "rgba(40, 255, 40)";
-    ctx.fillText(foods, rect[0], rect[1]);
+    // 食べられない食材が入っている場合、表示上限は暫定9件(後で調整)
+    // 多分下のコメントアウトのコードで上手くいく
+    const width = 100;
+    const height = 100;
+    const numTimes = 9;
+    const kaisuu = numTimes > imageArray.length? imageArray.length: numTimes;
+    let count = 0;
+    for (let i=0; i<3; i++) {
+    	for (let j=0; j<3; j++) {
+    		if (count < kaisuu) {
+    			ctx.drawImage(imageArray[count], rect[0] + width*i, rect[1] + height*j, width, height);
+        		count++;
+    		}
+    	}
+    }
 }
 
-let time = 0;
-const timeAllowed = 20;
-let text = "";
-
-Quagga.onDetected((result) => {
-    time = timeAllowed;
+Quagga.onDetected(async (result) => {
+    res = result;
+	time = timeAllowed;
     const barcode = result.codeResult.code;
     console.log(barcode);
-    checkBarcode(barcode);
-    drawText(result, text);
+    await checkBarcode(barcode);
 });
+
+let i = 0;
 
 Quagga.onProcessed((result) => {
     const ctx = Quagga.canvas.ctx.overlay;
@@ -111,6 +136,14 @@ Quagga.onProcessed((result) => {
 
     if (time <= 0) {
         ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
+        imageArray = [];
+        prevBarcode = "";
         time = 0;
     }
+
+	if (i === 0 && imageArray.length > 0) {
+        drawText();
+
+    }
+    i = (i+1)%frameRate;
 });
--- src/u_ar/ARController.java
+++ src/u_ar/ARController.java
@@ -1,17 +1,11 @@
 package u_ar;
 
 import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import bean.Foods;
-import dao.ProductFoods;
-import dao.RestFoodsDAO;
 import tool.CommonServlet;
 
 @WebServlet(urlPatterns={"/ajax-test"})
@@ -21,39 +15,43 @@ public class ARController extends CommonServlet {
     protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         resp.setContentType("application/json");
         PrintWriter out = resp.getWriter();
-        int barcode = Integer.parseInt(req.getParameter("barcode"));
+//        int barcode = Integer.parseInt(req.getParameter("barcode"));
 
-        HttpSession session = req.getSession();
-        // ユーザIDを取得し、登録されている制限食材を取得
-        // ユーザのログイン機能ができていないためコメントアウト
-        int userId = Integer.parseInt((String)session.getAttribute("u_id"));
-        List<Foods> restFoods = (new RestFoodsDAO()).getRestFoods(userId);
+//        HttpSession session = req.getSession();
+//        // ユーザIDを取得し、登録されている制限食材を取得
+//        // ユーザのログイン機能ができていないためコメントアウト
+//        int userId = Integer.parseInt((String)session.getAttribute("u_id"));
+//        List<Foods> restFoods = (new RestFoodsDAO()).getRestFoods(userId);
+//
+//        // barcodeから商品に入っている食材を取得
+//        ProductFoods pFoods = new ProductFoods();
+//        List<Foods> productFoods = pFoods.searchFoodsByJanCode(barcode);
+//
+//        // 制限食材と商品食材で重複しているものを調べる
+//        HashSet<Foods> set1 = new HashSet<>(restFoods);
+//        HashSet<Foods> set2 = new HashSet<>(productFoods);
+//        set1.retainAll(set2);
+//
+//        // 登録した食材がなかった場合の処理も考える
+//        if (set1.isEmpty()) {
+//            out.println("empty");
+//        } else {
+//            StringBuilder sBuilder = new StringBuilder();
+//            sBuilder.append("{\"classes\": [");
+//            // 食材名とicon_r, icon_gのpathを用意する(jsonの拡張)(面倒)
+//            for (Foods item: set1) {
+//                sBuilder.append("\"" + item + "\", ");
+//            }
+//            sBuilder.delete(sBuilder.length() - 2, sBuilder.length());
+//            sBuilder.append("]}");
+//
+//
+//            out.print(sBuilder.toString());
+//        }
 
-        // barcodeから商品に入っている食材を取得
-        ProductFoods pFoods = new ProductFoods();
-        List<Foods> productFoods = pFoods.searchFoodsByJanCode(barcode);
+        String string = "{\"paths\": [\"AR.png\", \"class.png\", \"csv.jpg\", \"gennzaiti.png\"]}";
 
-        // 制限食材と商品食材で重複しているものを調べる
-        HashSet<Foods> set1 = new HashSet<>(restFoods);
-        HashSet<Foods> set2 = new HashSet<>(productFoods);
-        set1.retainAll(set2);
-
-        // 登録した食材がなかった場合の処理も考える
-        if (set1.isEmpty()) {
-            out.println("empty");
-        } else {
-            StringBuilder sBuilder = new StringBuilder();
-            sBuilder.append("{\"classes\": [");
-            // 食材名とicon_r, icon_gのpathを用意する(jsonの拡張)(面倒)
-            for (Foods item: set1) {
-                sBuilder.append("\"" + item + "\", ");
-            }
-            sBuilder.delete(sBuilder.length() - 2, sBuilder.length());
-            sBuilder.append("]}");
-
-
-            out.print(sBuilder.toString());
-        }
+        out.print(string);
 
 
 
